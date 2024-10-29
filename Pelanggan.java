140035f629b44a61d258e06491d7fb29fd782a28
Uti2609 <justiciauti@gmail.com>
2024-10-29 18:39:54 +0700

    Add files via upload

---
+++ Pelanggan.java
@@ -0,0 +1,12 @@
+public class Pelanggan {
+    private String nama;
+
+    public Pelanggan(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+}
+
